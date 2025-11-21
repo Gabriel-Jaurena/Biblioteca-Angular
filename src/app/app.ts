175a30f50b32e34a746498eff328ba9c6ca8942b
Gabriel <gabriel.jaurena98@gmail.com>
2025-11-21 17:35:09 -0300

    first adds

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// src/app/app.ts
+import { Component } from '@angular/core';
+// 1. Importamos RouterLink además de RouterOutlet
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // Esto confirma que es Standalone
+  // 2. Añadimos RouterLink a la lista de imports
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('biblioteca');
+  // Ya no necesitamos la variable 'title' porque borraste su uso en el HTML
 }
