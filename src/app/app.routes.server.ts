ef81433195da2ad40710f053b3518375cbc16261
Gabriel <gabriel.jaurena98@gmail.com>
2025-11-25 23:00:37 -0300

    Fix: Instalar netlify-angular-runtime para SSR

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // 1. Excepciones para rutas dinámicas (Se generan en vivo en el servidor)
+  {
+    path: 'libros/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'sagas/:id',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
